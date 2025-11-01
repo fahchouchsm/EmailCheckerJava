a27868e06670351b450a834ec9c8eed88404c09b
Fahchouch Mohammed <fahchouchsm@gmail.com>
2025-11-01 20:50:36 +0100

    Improve email verification logic and update artifactId
    Enhanced the email verification method to add stricter checks for subdomain lengths and the number of '@' and '.' characters. Updated the Maven artifactId to 'EmailCheckerJava'. Added IntelliJ VCS configuration file.

--- src/main/java/org/MailVerification.java
+++ src/main/java/org/MailVerification.java
@@ -14,19 +14,33 @@ public class MailVerification {
     public boolean verifyEmail() {
         if(email == null || email.isEmpty()) return false;
         email = email.toLowerCase().trim();
-//        checking length
-        if (email.substring(0 ,email.indexOf('@')).length() < 3){
+
+        if (email.substring(0 ,email.indexOf('@')).length() < 3){ //check the name length is 2 char before @
             return false;
         }
+
         String domain = email.substring(email.indexOf('@'));
-        if(domain.substring(0, domain.indexOf('.')).length() < 3){
+        String subDomains = domain.substring(domain.indexOf('.') + 1);
+        String[] subDomainsArr = subDomains.split("\\.");
+
+        if (calcAChar(email, '@') != 1) { // check if there is one at
             return false;
         }
-        else if (calcAChar(email, '@') != 1) { //algorithm
-            return false;
-        }else if(calcAChar(email.substring(email.indexOf('@')), '.') != 1){
+
+        if(domain.substring(0, domain.indexOf('.')).length() < 2){ // check if there is 2 char between @ and .
             return false;
         }
+
+        if(calcAChar(subDomains, '.') == 0){ // if there is . in the domain
+            return false;
+        }
+
+        for(String c:subDomainsArr) { // checking length of subdomains 2 char
+            if(c == null || c.length() < 2){
+                return false;
+            }
+        }
+
         return true;
     }
 
@@ -38,14 +52,8 @@ public class MailVerification {
         return count;
     }
 
-
     public static void main(String[] args){
-        MailVerification m = new MailVerification("si.mofff0@qq.com");
-        String email = m.getEmail();
-        String domain = email.substring(email.indexOf('@'));
-        if(domain.substring(0, domain.indexOf('.')).length() < 3){
-            System.out.println("here");
-        }
+        MailVerification m = new MailVerification("si.mofff0@qq.com.ja");
 
         if (m.verifyEmail()) {
             System.out.println("all good");
